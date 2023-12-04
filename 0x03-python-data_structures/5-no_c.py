b7e1a99fb9d1331182b2e5578c561478a2f712ee
Ogechimak <ogechimac111@gmail.com>
2023-12-04 05:19:22 -0800

    Remove all character cC

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
+
 def no_c(my_string):
     new_string = []
-    for elements in my_string:
-        if elements not in 'cC':
-            new_string.append(elements)
-            return ''.join(new_string)
+    for string in my_string:
+        if string not in 'cC':
+            new_string.append(string)
+    return ''.join(new_string)
