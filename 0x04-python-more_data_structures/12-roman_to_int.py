fe004828d2bfc56f2d14517165faba52ea056a19
Ogechimak <ogechimac111@gmail.com>
2023-12-06 22:33:24 +0100

    Roman to integer

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
+
 def roman_to_int(roman_string):
     if not isinstance(roman_string, str) or roman_string is None:
         return 0
-    
+
     roman_dict = {
         'I': 1,
         'V': 5,
@@ -12,10 +13,10 @@ def roman_to_int(roman_string):
         'D': 500,
         'M': 1000
     }
-    
+
     result = 0
     prev_value = 0
-    
+
     for char in roman_string[::-1]:
         value = roman_dict[char]
         if value >= prev_value:
@@ -23,5 +24,5 @@ def roman_to_int(roman_string):
         else:
             result -= value
         prev_value = value
-    
+
     return result
