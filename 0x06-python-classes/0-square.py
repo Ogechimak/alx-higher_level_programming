c6e99eba697cd607bad287e52db877a8ba0b4256
Ogechimak <ogechimac111@gmail.com>
2023-12-20 20:54:20 +0100

    My first square

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+"""Square module: The class 'Square' contains a Square object"""
+
+
+class Square:
+    """ square class
+
+    The square class has no implementation.
+
+    """
+    pass
