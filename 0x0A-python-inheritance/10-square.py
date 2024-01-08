3cec163762ae1bc158e7bf773b51920ffd0cfa4f
Ogechimak <ogechimac111@gmail.com>
2024-01-08 16:53:53 +0100

    Square

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -1,6 +1,14 @@
 #!/usr/bin/python3
+
+"""
+    a module that contains a public instance method
+"""
+
+
 bg = __import__('9-rectangle').Rectangle
 """ importation of Base Rectangle class """
+
+
 class Square(bg):
     """ Base rectangle class with only one method  """
     def __init__(self, size):
