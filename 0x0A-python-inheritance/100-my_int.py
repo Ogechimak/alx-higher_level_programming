d391932abd0322fe9ed1045bee9246779b85a6f0
Ogechimak <ogechimac111@gmail.com>
2024-01-08 17:04:28 +0100

    My int

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 """A class that inherits from int"""
 class MyInt(int):
-        def __eq__(self, other):
-            return not super().__eq__(other)
-        def __ne__(self, other):
-            return not super().__ne__(other)
+    """class that inherits from int"""
+    def __eq__(self, other):
+        """method is overridden to invert the behavior of the '==' operator"""
+        return not super().__eq__(other)
+    def __ne__(self, other):
+        """methd is overridden to invert the behavior of the '!=' operator"""
+        return not super().__ne__(other)
