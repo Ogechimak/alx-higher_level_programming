863422977a5d22ce75cb80076131dbeaa1d2d744
Ogechimak <ogechimac111@gmail.com>
2024-01-08 21:52:34 +0100

    Square

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -1,21 +1,19 @@
 #!/usr/bin/python3
-
-"""
-    a module that contains a public instance method
-"""
+"""Module to create a class"""
 
 
-bg = __import__('9-rectangle').Rectangle
-""" importation of Base Rectangle class """
-
-
-class Square(bg):
-    """ Base rectangle class with only one method  """
+class Square(__import__('9-rectangle').Rectangle):
+    """Class that inherits from Rectangle (9-rectangle.py)"""
     def __init__(self, size):
-        super().__init__(size, size)
+        """Instantiation with size"""
+        self.integer_validator("size", size)
         self.__size = size
-        """constructor"""
+        super().__init__(size, size)
+
+    def area(self):
+        """Returns the area of the square"""
+        return self.__size ** 2
 
     def __str__(self):
-        """ string implementation of the instance """
-        return f"[Square] {self.__size}/{self.__size}"
+        """Returns a string"""
+        return "[Square] {}/{}".format(self.__size, self.__size)
