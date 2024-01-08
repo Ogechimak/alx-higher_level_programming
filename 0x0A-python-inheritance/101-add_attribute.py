eddf91fff171bc9f82404ddfcff454b1c61022c9
Ogechimak <ogechimac111@gmail.com>
2024-01-08 17:07:11 +0100

    My int

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 """ function that adds a new attribute to an object if it’s possible"""
-
 def add_attribute(obj, attr_name, attr_value):
     """check if a new attribute can be added"""
     if not hasattr(obj, '__dict__') and not hasattr(type(obj), '__slots__'):
