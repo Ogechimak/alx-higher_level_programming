a030c48a8d275a052ce32f33c7a57cd0c6c9d163
Ogechimak <ogechimac111@gmail.com>
2024-01-08 16:36:37 +0100

    sub class

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,6 +1,16 @@
 #!/usr/bin/python3
-""" function that returns True if the object is an instance of a class that inherited (directly or indirectly) from the specified class ; otherwise False."""
-def inherits_from(obj, a_class):
-    if type(obj) is a_class:
-        return False
-    return issubclass(type(obj), a_class)
+
+"""
+#############################################
+module containing is_kind_of_class function
+##############################################
+"""
+
+
+def is_kind_of_class(obj, a_class):
+    """
+        function that returns True if the object is an instance of,
+        or if the object is an instance of a class that inherited from,
+        the specified class ; otherwise False.
+    """
+    return isinstance(obj, a_class)
