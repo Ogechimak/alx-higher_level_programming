6acf295c99692af47249391d485ee41135e0c266
Ogechimak <ogechimac111@gmail.com>
2024-01-09 14:51:32 +0100

    Student of JSON

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -1,23 +1,28 @@
 #!/usr/bin/python3
-"""method for student creation"""
+'''
+file: 11-student.py
+Classes:
+-> Student
+'''
 
 
 class Student:
-    """Student obj, interesting how you don't have to directly
-    test for strings in a loop, python is weird"""
+    ''' Student class '''
 
     def __init__(self, first_name, last_name, age):
-		"""Construction method"""
+        ''' Constructor method '''
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
-		"""Method that returns directory descriptin with filter"""
-        if attrs is None:
-            return self.__dict__
-        new_dictionary = {}
-        for key, value in self.__dict__.items():
-            if key in attrs:
-                new_dictionary[key] = value
-        return new_dictionary
+        ''' Method that returns directory description with filter '''
+
+        if isinstance(attrs, list) and all(isinstance(attr, str)
+                                           for attr in attrs):
+            res = {}
+            for i in attrs:
+                if i in self.__dict__:
+                    res[i] = self.__dict__[i]
+            return res
+        return self.__dict__
