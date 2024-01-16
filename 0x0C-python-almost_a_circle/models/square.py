0ebf4b2ad80c7501ee6a8a8ffb45f1020e197976
Ogechimak <ogechimac111@gmail.com>
2024-01-16 16:46:43 +0100

    square class

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -8,6 +8,11 @@ class Square(Rectangle):
 
     def __init__(self, size, x=0, y=0, id=None):
         """Initialize a new Square.
+        Args:
+            size (int): The size of the new Square.
+            x (int): The x coordinate of the new Square.
+            y (int): The y coordinate of the new Square.
+            id (int): The identity of the new Square.
         """
         super().__init__(size, size, x, y, id)
 
@@ -23,9 +28,6 @@ class Square(Rectangle):
 
     def update(self, *args, **kwargs):
         """Update the Square.
-        Args:
-            *args (ints): New attribute values.
-            **kwargs (dict): New key/value pairs of attributes.
         """
         if args and len(args) != 0:
             a = 0
