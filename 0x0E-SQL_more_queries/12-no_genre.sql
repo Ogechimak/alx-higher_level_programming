44d7ff1a220fcaabd06bc8b763c9d396af7802bb
Ogechimak <ogechimac111@gmail.com>
2024-02-19 13:36:59 +0100

    more queries

--- 0x0E-SQL_more_queries/12-no_genre.sql
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -1,7 +1,7 @@
--- Script that lists all cities contained in the database
---join cities and states
-SELECT tv_shows.title, tv_show_genres.genre_id
-FROM tv_shows
-LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
-WHERE tv_show_genres.genre_id is NULL
-ORDER BY tv_shows.title;
+-- script that lists all shows contained in the database hbtn_0d_tvshows.
+-- USE LEFT join
+SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_shows
+LEFT JOIN tv_show_genres
+ON tv_shows.id = tv_show_genres.show_id
+WHERE tv_show_genres.show_id is NULL
+ORDER BY tv_shows.title, tv_show_genres.genre_id ASC;
