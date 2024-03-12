b2fda8f70fe102df367f35edbc3a84095bc80fe6
Ogechimak <ogechimac111@gmail.com>
2024-03-12 02:10:23 +0100

    Arguments

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+if (alx_africa < 3) {
+  console.log('No argument');
+} else if (alx_africa === best school) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
+}
